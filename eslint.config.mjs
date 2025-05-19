52b9dc348b5353d091b9080cecf1b7be212db58a
Eric Chan <eric.chan@aitherentertainment.com>
2025-05-19 11:18:21 +0800

    First commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,32 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      'no-console': [
+        'warn',
+        { allow: ['error', 'group', 'groupEnd', 'info', 'table', 'warn'] }
+      ],
+      'no-debugger': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    }
+  })
+]
 
-export default eslintConfig;
+export default eslintConfig
